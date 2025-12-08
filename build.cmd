71accadbb59ca927551846723796f731700e352d
Nico Egerer <Nico.Egerer@reiser-st.com>
2025-12-08 15:18:15 +0100

    website fix

--- build.cmd
+++ build.cmd
@@ -2,4 +2,5 @@
 :: .\emsdk_env.ps1
 
 
-emcc -o C:/GameDev/Raylib/JumpJump/Web/index.html main.cpp Background.cpp Platform.cpp Character.cpp -Wall -std=c++14 -D_DEFAULT_SOURCE -Wno-missing-braces -Wunused-result -Os -I. -I C:/raylib/raylib/src -I C:/raylib/raylib/src/external -L. -L C:/raylib/raylib/src -s USE_GLFW=3 -s ASYNCIFY -s TOTAL_MEMORY=67108864 -s FORCE_FILESYSTEM=1 --preload-file Background --preload-file Character --preload-file Pads --shell-file C:/GameDev/Raylib/JumpJump/Web-Template/index.html C:/raylib/raylib/src/web/libraylib.a -DPLATFORM_WEB -s EXPORTED_FUNCTIONS="['_free', '_malloc', '_main']" -s EXPORTED_RUNTIME_METHODS=ccall
+emcc main.cpp Background.cpp Platform.cpp Character.cpp -Wall -std=c++14 -D_DEFAULT_SOURCE -Wno-missing-braces -Wunused-result -Os -I. -I C:/raylib/raylib/src -I C:/raylib/raylib/src/external -L. -L C:/raylib/raylib/src -s USE_GLFW=3 -s ASYNCIFY -s TOTAL_MEMORY=67108864 -s FORCE_FILESYSTEM=1 --preload-file Background --preload-file Character --preload-file Pads C:/raylib/raylib/src/web/libraylib.a -DPLATFORM_WEB -s EXPORTED_FUNCTIONS="['_free','_malloc','_main']" -s EXPORTED_RUNTIME_METHODS=ccall -o C:/GameDev/Raylib/JumpJump/Web/index.js
+
